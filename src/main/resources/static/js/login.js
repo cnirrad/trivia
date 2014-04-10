c38dfc027f5801c3eae649bb5b5c43f8456f8651
Darrin Collins <darrin.collins@tdameritrade.com>
2014-04-10 13:35:50 -0500

    Store the users token in a cookie

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {  
 	
+	var createCookie = function(name, value, days) {
+	    var expires;
+
+	    if (days) {
+	        var date = new Date();
+	        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+	        expires = "; expires=" + date.toGMTString();
+	    } else {
+	        expires = "";
+	    }
+	    document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/";
+	}
+
+	var readCookie = function(name) {
+	    var nameEQ = escape(name) + "=";
+	    var ca = document.cookie.split(';');
+	    for (var i = 0; i < ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+	        if (c.indexOf(nameEQ) === 0) return unescape(c.substring(nameEQ.length, c.length));
+	    }
+	    return null;
+	}
+	
 	// approximation of a GUID taken from 
 	// http://stackoverflow.com/a/105074
 	var guid = function() {
@@ -12,8 +36,11 @@ $(document).ready(function() {
 		         s4() + '-' + s4() + s4() + s4();
 		}
 	
-	//TODO: read from cookie, create and save if not present
-	var password = guid();
+	var password = readCookie('triviaName');
+	if (password == null) {
+		password = guid();
+		createCookie('triviaName', password, 1);
+	}
 	
 	// Disable the button until the user has entered a name
 	$('#username').on('keyup blur', function(){
